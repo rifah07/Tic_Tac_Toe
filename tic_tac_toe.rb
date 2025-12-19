6c0c358cc3f69afbd9c9d3bc3c8b100c11a21bd4
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-12-19 23:51:48 +0600

    updated

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -10,9 +10,57 @@ module TicTacToe
 
       @current_player_id = 0
       @players = [player_1_class.new(self, 'X'), player_2_class.new(self, 'O')]
-      puts "#{@current_player_id} goes first"
+      puts "#{@current_player} goes first"
     end
 
     attr_reader :board, :current_player_id
+
+    def play
+      loop do
+        place_player_marker(current_player)
+
+        if player_has_won?(current_player)
+          puts "#{current_player} wins!"
+          print_board
+          return
+        elsif board_full?
+          puts "It's a draw!"
+          print_board
+          return
+        end
+
+        switch_players!
+      end
+    end
+
+    def free_positions
+      (1..9).select {|position| @board[position].nil?}
+    end
+
+    def place_player_marker(player)
+      position = player.select_position!
+      puts "#{player} selects #{player.marker} position #{position}"
+      @board[position] = player.marker
+    end
+
+    def player_has_won?
+
+    end
+
+    def board_full?
+
+    end
+
+    def other_player_id
+
+    end
+
+    def switch_players!
+
+    end
+
+    def select_position!
+
+    end
   end
 end
