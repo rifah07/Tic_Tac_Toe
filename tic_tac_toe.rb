94e645b9fc46385576bcedd47a5e8c82483b0b4c
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-12-22 21:41:43 +0600

    HumanPlayer class added

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -92,4 +92,25 @@ module TicTacToe
     end
     attr_reader :marker
   end
+
+  # HumanPlayer inherits from Player class
+  class HumanPlayer < Player
+    def select_position!
+      @game.print_board
+
+      loop do
+        print "Select your #{marker} position: "
+        selection = gets.to_i
+        return selection if @game.free_positions.include?(selection)
+
+        puts "Position #{selection} is not available! Try again."
+      end
+    end
+
+    def to_s
+      'Human'
+    end
+  end
+
+
 end
